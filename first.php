7191b0adba46b5207167db2f52ce39f7a10b87af
mdtawshif <tawshifmostafa15@gmail.com>
2023-08-27 21:38:09 +0600

    inheritance

--- first.php
+++ first.php
@@ -7,33 +7,62 @@
 </head>
 <body>
 <?php
-class User {
+   class User
+   {
 
-   public $username;
-   private $email;
+      public $username;
+      private $email;
+
+      public function __construct($username, $email)
+      {
+         $this->username = $username;
+         $this->email = $email;
+      }
+
+      public function addFriend()
+      {
+         return "$this->email  added a new friend";
+
+      }
+
+      public function getEmail()
+      {
+         return $this->email;
+      }
+
+      public function setEmail($email)
+      {
+         if (strpos($email, '@') > -1) {
+            $this->email = $email;
+         }
+      }
+   }
+   class AdminUser extends User
+   {
+
+   }
+   $userOne = new user('tawshif', 'assmec.com');
+   $userTwo = new user('alim', 'addnessmec.com');
+   $userThree = new AdminUser('Joyboy', 'onepiece@gmail.com');
+
+   echo $userThree-> username .'<br>';
+   echo $userThree-> getEmail() .'<br>';
+
+
+
+   // $userOne->setEmail('mostafatawshif@gmail.com');
+   // echo $userOne->getEmail() . '<br>';
+   // echo $userTwo->getEmail();
    
- public function __construct($username, $email){
-   $this->username =$username;
-   $this->email = $email;
- }
+   // echo $userOne->username . '<br>';
+   // echo $userOne->email . '<br>';
+   // echo $userOne->addFriend() . '<br>';
+   
 
- public function addFriend(){
-    return "$this->email  added a new friend";
-    
- }
-}
-
-$userOne = new user('tawshif', 'assmec.com');
-$userTwo = new user('alim', 'addnessmec.com');
-
-// echo $userOne->username . '<br>';
-// echo $userOne->email . '<br>';
-echo $userOne->addFriend() . '<br>';
-
-
-// echo $userTwo->username . '<br>';
-// echo $userTwo->email . '<br>';
-// echo $userTwo->addFriend() . '<br>';
+   // echo $userTwo->username . '<br>';
+   // echo $userTwo->email . '<br>';
+   // echo $userTwo->addFriend() . '<br>';
+   
 
 
 
